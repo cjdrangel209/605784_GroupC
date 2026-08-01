e9e7658d21a68fe28f15055bece519a3f1ddcb56
cjdrangel209 <cjdrangel209@gmail.com>
2026-08-01 14:54:03 -0400

    Updated javadocs for MaintService

--- src/main/java/edu/jhu/mrm/service/MaintRequestService.java
+++ src/main/java/edu/jhu/mrm/service/MaintRequestService.java
@@ -18,8 +18,8 @@ import edu.jhu.mrm.repository.MaintRequestRepository;
 /**
  * File: MaintRequestService.java
  * Service layer for MaintRequest
- * Author: Cory Drangel and Matthew Kim
  * 
+ * @author Cory Drangel and Matthew Kim
  */
 @Service
 public class MaintRequestService {
@@ -30,12 +30,26 @@ public class MaintRequestService {
 	@Autowired
 	private AccountRepository accountRepository;
 	
+	/**
+	 * Utility function to get the current user's account
+	 * 
+	 * @param username
+	 * 			Username to get the Account of
+	 * @return An account
+	 */
 	private Account getByUsername(String username) {
         return accountRepository.findByUsername(username)
                 .orElseThrow(() -> new ResponseStatusException(
                         HttpStatus.NOT_FOUND, "Account not found with username: " + username));
     }
 	
+	/**
+	 * Creates a new MaintRequest
+	 * 
+	 * @param request
+	 * 			The information to use for the new maintenance request object
+	 * @return The new MaintRequest object
+	 */
 	public MaintRequest create(MaintRequest request) {
 		MaintRequest newRequest = new MaintRequest();
 		newRequest.setAccountId(request.getAccountId());
@@ -50,14 +64,36 @@ public class MaintRequestService {
 		return requestRepository.save(newRequest);
 	}
 	
+	/**
+	 * Finds all MaintRequest objects associated with an account ID
+	 * 
+	 * @param id
+	 * 			The accountId to search by
+	 * @return List of MaintRequest objects
+	 */
 	public List<MaintRequest> getByAccountId(String id) {
 		return requestRepository.findByAccountId(id);
 	}
 	
+	/**
+	 * Finds all MaintRequest objects currently in database
+	 * 
+	 * @return List of MaintRequest objects
+	 */
 	public List<MaintRequest> getAll() {
 		return requestRepository.findAll();
 	}
 	
+	/**
+	 * Gets a single MaintRequest object. Tenants can only search for their own requests.
+	 * Workers can only search for requests assigned to them.
+	 * 
+	 * @param requestId
+	 * 			The ID of the MaintRequest to search for
+	 * @param requestorUsername
+	 * 			The user name of the person searching for the maintenance request
+	 * @return A MaintRequest object
+	 */
 	public MaintRequest getByRequestId(String requestId, String requestorUsername) {
 		
 		Account requestor = getByUsername(requestorUsername);
@@ -80,6 +116,17 @@ public class MaintRequestService {
 		return request;
 	}
 	
+	/**
+	 * Updates a MaintRequest object.
+	 * 
+	 * @param id
+	 * 			The ID for the MaintRequest to update
+	 * @param update
+	 * 			The updated information to utilize
+	 * @param requestorUsername
+	 * 			The user name of the person updating the maintenance request
+	 * @return A MaintRequest object that has been updated
+	 */
 	public MaintRequest update(String id, MaintRequest update, String requestorUsername) {
 		
 		MaintRequest newRequest = getByRequestId(id, requestorUsername);
@@ -140,6 +187,17 @@ public class MaintRequestService {
 		return newRequest;
 	}
 	
+	/**
+	 * Assigns a worker to a MaintRequest object
+	 * 
+	 * @param id
+	 * 			The ID of the MaintRequest object
+	 * @param workerId
+	 * 			The ID for the associated worker
+	 * @param requestorUsername
+	 * 			The user name for the person assigning the worker to the MaintRequest 
+	 * @return A MaintRequest object with an assigned worker
+	 */
 	public MaintRequest assign(String id, String workerId, String requestorUsername) {
 		MaintRequest request = getByRequestId(id, requestorUsername);
 		
